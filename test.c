a55959274ffde19316f20a01157af31ba4d1b78f
ApotoxinElish <19079181d@connect.polyu.hk>
2021-10-04 00:49:43 +0800

    Update

--- test.c
+++ test.c
@@ -9,9 +9,9 @@ void main(void)
 {
     int i, j;
 
-    for (j = MIN; j <= MAXTABLE; j += STEP)
+    for (j = MIN; j < MAXTABLE; j += STEP)
     {
-        for (i = MIN; i <= MAXINDEX; i = STEP)
+        for (i = MIN; i <= MAXINDEX; i += STEP)
             printf("%3d * %3d = %3d\n", i, j, i * j);
         printf("\n---------------\n\n");
     }
